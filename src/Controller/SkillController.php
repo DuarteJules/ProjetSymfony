47038b7809173bef314a5b1cb23e9dfd440bdc65
Jerance <michael.jerance@gmail.com>
2022-11-08 14:42:56 +0100

    Form Skill add done in bdd

---
+++ src/Controller/SkillController.php
@@ -0,0 +1,35 @@
+<?php
+// src/Controller/SkillController.php
+namespace App\Controller;
+
+use App\Entity\Skill;
+use App\Form\Type\SkillType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class SkillController extends AbstractController
+{
+
+    #[Route(path: '/skills')]
+    public function new(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $skill = new Skill();
+
+        $form = $this->createForm(SkillType::class, $skill);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $objectManager = $managerRegistry->getManager();
+            $objectManager->persist($skill);
+            $objectManager->flush();
+        }
+
+        return $this->renderForm('skill/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
