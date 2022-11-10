65dd86a1700d2bd5f30269b578735acc69b02d88
David Roquain <davidroquain03@gmail.com>
2022-11-10 07:10:41 +0100

    Envoie du mail au candidat retenu pour l'offre

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Candidate;
+use App\Entity\Candidature;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
@@ -9,16 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
-    #[Route('/email/{mail}' , name: 'mailer_show')]
-    public function sendEmail(MailerInterface $mailer,string $mail): Response
+    #[Route('/email/{idCandidate}/{id}' , name: 'mailer_show')]
+    public function sendEmail(ManagerRegistry $doctrine,MailerInterface $mailer,int $idCandidate, int $id): Response
     {
+        $candidature = $doctrine->getRepository(Candidature::class)->find($id);
+        $candidate = $doctrine->getRepository(Candidate::class)->find($idCandidate);
+        $job = $candidature->getJob();
+        $namecompany = $job->getCompany()->getname();
+        $namejob = $job->getName();
+        $mail = $candidate -> getEmail();
         $email = (new Email())
             ->from('CodingRecrute@gmail.com')
             ->to($mail)
             ->subject('Réponse à candidature')
-            ->text("Vous avez été retenu pour l'offre");
+            ->text("Vous avez été retenu pour l'offre intitulé " . $namejob . " posté par " . $namecompany . ".");
 
         $mailer->send($email);
-        return $this->render('email/confirm.html.twig');
+        return $this->redirectToRoute('details_job',['id' => $job->getId()]);
     }
 }
